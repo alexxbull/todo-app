b1012e5ae1f2a3257b5028c82a7bc8bb89abf888
Alexx Bull <alexxbull@outlook.com>
2020-10-05 03:32:45 -0700

    load todo items from data stored in local storage

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // styles
 import classes from './TodoList.module.css'
@@ -8,12 +8,35 @@ import TodoItem from './TodoItem/TodoItem.js'
 import AddTodoItem from './AddTodoItem/AddTodoItem.js'
 
 const TodoList = props => {
+    useEffect(() => {
+        if (localStorage['tasks']) {
+            const jsonData = JSON.parse(localStorage.getItem('tasks'))
+            const loadedTasks = []
+
+            for (const task of jsonData) {
+                loadedTasks.push(task)
+            }
+
+            setTasks(loadedTasks)
+        }
+    }, [])
+
+    const [tasks, setTasks] = useState([])
+
+    const todoItems = tasks.map(task => {
+        return (
+            <TodoItem
+                content={task.content}
+                done={task.done}
+            />
+        )
+    })
 
     return (
         <div className={classes.TodoList}>
             Todo
             <AddTodoItem />
-            <TodoItem />
+            {todoItems}
         </div>
     )
 }
